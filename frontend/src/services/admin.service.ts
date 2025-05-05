816b2b69476958a7215750e76328cae0172390c4
TruongThanhTamb26 <tam.truongb26@hcmut.edu.vn>
2025-05-05 14:36:47 +0700

    FINAL

--- frontend/src/services/admin.service.ts
+++ frontend/src/services/admin.service.ts
@@ -333,7 +333,6 @@ export const adminService = {
                     ...getAuthHeader()
                 },
             });
-            console.log(response.data);
             return response.data;
         } catch (error: any) {
             const errorMessage = error.response?.data?.message || 'Không thể check-in';
